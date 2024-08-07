fa30f04a226cee2ea98d24f05f47825ffb0981d0
刘硕 <liushuo@zetyun.com>
2024-08-08 07:42:19 +0800

    redis
    Signed-off-by: 刘硕 <liushuo@zetyun.com>

--- docker-install-redis.sh
+++ docker-install-redis.sh
@@ -4,4 +4,4 @@ homePath=$(pwd)
 dataPath="$homePath/data/redis"
 cd -
 #docker run -itd --name myRedis -p 6379:6379 --restart=always  redis --requirepass"123456"
-docker run -itd --name myRedis -p 6379:6379 --restart=always -v $dataPath:/data redis:7.0
+docker run -itd --name myRedis -p 6379:6379 --restart=always -v $dataPath:/data registry.cn-hangzhou.aliyuncs.com/acejilam/redis:7.0
