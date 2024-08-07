d0097d5673abaf65e2544c98a8cc71156279bc2b
刘硕 <liushuo@zetyun.com>
2024-08-07 23:09:47 +0800

    server
    Signed-off-by: 刘硕 <liushuo@zetyun.com>

--- customer_script.sh
+++ customer_script.sh
@@ -154,3 +154,5 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 
 alias company_proxy='export https_proxy=http://172.20.3.242:7890 http_proxy=http://172.20.3.242:7890 all_proxy=socks5://172.20.3.242:7890'
 alias ks='kubectl get pods -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace,RESOURCE_LIMIT:.spec.containers[*].resources.limits -A |grep -v none'
+
+[[ -s "/Users/acejilam/.gvm/scripts/gvm" ]] && source "/Users/acejilam/.gvm/scripts/gvm"
