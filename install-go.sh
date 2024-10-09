90b2f888ed37c2196159793d465c9733e386880b
刘硕 <liushuo@zetyun.com>
2024-10-09 18:03:14 +0800

    2024年10月 9日 星期三 18时03分14秒 CST

--- install-go.sh
+++ install-go.sh
@@ -1,17 +1,21 @@
-version=1.18
-
 rm -rf /usr/local/go*
-yum install wget vim -y
-wget https://golang.google.cn/dl/go$version.linux-amd64.tar.gz
+rm -rf ./go*
+yum install wget vim -y || apt install wget vim -y
+
+version=1.22.0
 mkdir /usr/local/go$version
-tar -xvf go$version.linux-amd64.tar.gz -C /usr/local/go$version --strip-components 1
 
-mkdir -p ~/go/{bin,src,pkg}
+ARCH=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 
+wget https://golang.google.cn/dl/go$version.linux-$ARCH.tar.gz
+tar -xvf go$version.linux-$ARCH.tar.gz -C /usr/local/go$version --strip-components 1
+rm -rf go$version.linux-$ARCH.tar.gz
+mkdir -p ~/.go/{bin,src,pkg}
+chmod -R 777 /usr/local/go$version
 cat <<EOF >>/etc/profile
 
 export GOROOT="/usr/local/go$version"
-export GOPATH=\$HOME/go  #工作地址路径
+export GOPATH=\$HOME/.go  #工作地址路径
 export GOBIN=\$GOROOT/bin
 export PATH=\$PATH:\$GOBIN
 EOF
@@ -20,3 +24,8 @@ go version
 go env
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
+go env -w GOFLAGS="-buildvcs=false"
+go env -w CGO_ENABLED="1"
+
+go install github.com/trzsz/trzsz-go/cmd/...@latest
+go install github.com/go-delve/delve/cmd/dlv@master
---
+++ install-rust-tools.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# https://github.com/taiki-e/cargo-llvm-cov?tab=readme-ov-file#installation
+cargo +stable install cargo-llvm-cov --locked
+
+# https://github.com/cargo-bins/cargo-binstall
+brew install cargo-binstall
+
+# https://github.com/nextest-rs/nextest
+brew install cargo-nextest
