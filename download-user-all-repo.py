a32a1b53bb6bf50b7ed3c5b01be1ac9a2b92189f
刘硕 <liushuo@zetyun.com>
2024-10-12 17:18:22 +0800

    feature: download

---
+++ download-user-all-repo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import json
+import os
+import sys, subprocess
+
+print(sys.argv)
+if len(sys.argv) < 3:
+    print('who?={users|orgs} type={users|orgs} ')
+    sys.exit(1)
+who = sys.argv[1]
+_type = sys.argv[2]
+
+for page in range(100):
+    print(f'-------------> {page}')
+    cmd = f'curl -s https://api.github.com/{_type}/{who}/repos?page={page}&per_page=1000 | grep -e \'clone_url*\' | cut -d \\" -f 4  '
+    res = subprocess.getoutput(cmd)
+    for item in json.loads(res):
+        repo = item['clone_url']
+        name = repo.split('/')[-1].split('.')[0]
+        if name in sys.argv[3:]:
+            continue
+        cmd = f'git clone {repo} || echo {repo} exists'
+        os.system(cmd)
