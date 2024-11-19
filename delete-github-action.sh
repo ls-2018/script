e41b6ea72b6d47e052bf7ce80779841b85d124d4
acejilam <acejilam@gmail.com>
2024-11-19 23:06:41 +0800

    github action

--- delete-github-action.sh
+++ delete-github-action.sh
@@ -1,5 +1,8 @@
-OWNER=${1-lsutils}
-REPOSITORY=${2-/root/sync}
+#! /usr/bin/env bash
+set -ex
 
-gh api -X GET "/repos/$OWNER/$REPOSITORY/actions/runs?per_page=3000" | jq '.workflow_runs[1:] | .[].id' | xargs -I{} sh -c 'echo {} && gh api --silent -X POST repos/$OWNER/$REPOSITORY/actions/runs/{}/cancel'
-gh api -X GET "/repos/$OWNER/$REPOSITORY/actions/runs?per_page=3000" | jq '.workflow_runs[1:] | .[].id' | xargs -I{} sh -c 'echo {} && gh api --silent -X DELETE /repos/$OWNER/$REPOSITORY/actions/runs/{}'
+export OWNER=${1-lsutils}
+export REPOSITORY=${2-sync}
+
+gh api -X GET /repos/$OWNER/$REPOSITORY/actions/runs?per_page=3000 | jq '.workflow_runs[1:] | .[].id' | xargs -I{} sh -c 'echo {} && gh api --silent -X DELETE /repos/$OWNER/$REPOSITORY/actions/runs/{}'
+gh api -X GET /repos/$OWNER/$REPOSITORY/actions/runs?per_page=3000 | jq '.workflow_runs[1:] | .[].id' | xargs -I{} sh -c 'echo {} && gh api --silent -X POST repos/$OWNER/$REPOSITORY/actions/runs/{}/cancel'
