9017c0f643c40c21c13d55f37036215f5b5d671b
acejilam <acejilam@gmail.com>
2024-11-20 21:40:12 +0800

    ebpf

--- init-ebpf.sh
+++ init-ebpf.sh
@@ -8,10 +8,9 @@ cp -rf ~/script/learning-ebpf.yaml /tmp/$name.yaml
 gsed -i "s#PWD#$(pwd)#" /tmp/$name.yaml
 gsed -i "s#HOSTNAME#${name}#" /tmp/$name.yaml
 
-
 limactl start /tmp/$name.yaml # --debug --log-level=debug
 
 echo ✈️✈️✈️✈️✈️✈️✈️✈️✈️✈️✈️✈️✈️✈️
 
-echo limactl shell $name sudo bash -c "cd $(pwd) && bash"
+echo "limactl shell $name sudo bash -c 'cd $(pwd) && bash'"
 limactl shell $name sudo bash -c "cd $(pwd) && bash"
