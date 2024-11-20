60e25d5ca8651008c0d8be05b1e25ddc3c38d4a3
acejilam <acejilam@gmail.com>
2024-11-20 17:10:53 +0800

    ebpf

--- init-ebpf.sh
+++ init-ebpf.sh
@@ -8,7 +8,9 @@ limactl delete $name -f
 cp -rf ~/script/learning-ebpf.yaml /tmp/$name.yaml
 
 gsed -i "s#PWD#$(pwd)#" /tmp/$name.yaml
+gsed -i "s#HOSTNAME#${name}#" /tmp/$name.yaml
+
 
 limactl start /tmp/$name.yaml # --debug --log-level=debug
 
-limactl shell $name
+limactl shell $name sudo bash -c "cd $(pwd) && bash"
