a9491dd710053e190f5f02ac41f9eb3517e3ac88
acejilam <acejilam@gmail.com>
2024-11-20 19:53:40 +0800

    ebpf

--- linux-install-go.sh
+++ linux-install-go.sh
@@ -10,15 +10,22 @@ ARCH=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 wget https://golang.google.cn/dl/go$version.linux-$ARCH.tar.gz
 tar -xvf go$version.linux-$ARCH.tar.gz -C /usr/local/go$version --strip-components 1
 rm -rf go$version.linux-$ARCH.tar.gz
-mkdir -p ~/.go/{bin,src,pkg}
+mkdir -p ~/.gopath/{bin,src,pkg}
 chmod -R 777 /usr/local/go$version
-cat <<EOF >>~/.bashrc
-
+cat <<EOF >> /etc/profile
 export GOROOT="/usr/local/go$version"
 export GOPATH=\$HOME/.gopath  #工作地址路径
 export GOBIN=\$GOROOT/bin
 export PATH=\$PATH:\$GOBIN
 EOF
+
+cat <<EOF >> ~/.bashrc
+export GOROOT="/usr/local/go$version"
+export GOPATH=\$HOME/.gopath  #工作地址路径
+export GOBIN=\$GOROOT/bin
+export PATH=\$PATH:\$GOBIN
+EOF
+
 source /etc/profile
 go version
 go env
