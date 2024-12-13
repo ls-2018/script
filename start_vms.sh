49e232f2bf36178eba0dcb468da3d05dd8bbc47e
acejilam <acejilam@gmail.com>
2024-12-13 11:23:39 +0800

    set -ex

--- start_vms.sh
+++ start_vms.sh
@@ -76,9 +76,9 @@ Vagrant.configure("2") do |config|
 end
 
 EOF
-
+set -ex
 rm -rf .vagrant
-pkill -9 vagrant
+
 vagrant global-status --prune
 
 # 定义虚拟机名称数组
@@ -86,6 +86,7 @@ vms=("1804" "2004" "2404")
 
 # 使用并发执行 vagrant up 命令
 for vm in "${vms[@]}"; do
+  vagrant destroy "$vm" --force
   vagrant up "$vm" &
 done
 
