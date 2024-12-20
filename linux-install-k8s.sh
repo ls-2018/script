b80a757ffa1494340ac72dd803cd937f2f1ac079
acejilam <acejilam@gmail.com>
2024-12-20 23:10:59 +0800

    ebpf

--- linux-install-k8s.sh
+++ linux-install-k8s.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env zsh
 
-export VERSION=v4.3.0
-curl -sfL https://cf.ghproxy.cc/https://raw.githubusercontent.com/labring/sealos/${VERSION}/scripts/install.sh | sh -s ${VERSION} labring/sealos
-unset https_proxy
+export VERSION=4.3.0
+ARCH=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 
-rm -rf ~/.sealos/
+curl -sfL https://cf.ghproxy.cc/https://github.com/labring/sealos/releases/download/v${VERSION}/sealos_${VERSION}_linux_${ARCH}.tar.gz | tar -zxvf - -C /usr/bin/
 # sealos reset
 
 sealos run registry.cn-hangzhou.aliyuncs.com/acejilam/kubernetes-docker:v1.25.16 \
