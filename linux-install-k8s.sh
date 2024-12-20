35554649c60c394d73e41d83460bd825b324cba7
acejilam <acejilam@gmail.com>
2024-12-20 23:52:41 +0800

    ebpf

--- linux-install-k8s.sh
+++ linux-install-k8s.sh
@@ -9,3 +9,8 @@ curl -sfL https://cf.ghproxy.cc/https://github.com/labring/sealos/releases/downl
 sealos run registry.cn-hangzhou.aliyuncs.com/acejilam/kubernetes-docker:v1.25.16 \
     registry.cn-hangzhou.aliyuncs.com/acejilam/helm:v3.8.2 \
     --masters 192.168.33.12 --passwd 'root'
+
+curl -sfL https://cf.ghproxy.cc/https://github.com/cilium/cilium-cli/releases/download/v0.16.22/cilium-darwin-arm64.tar.gz | tar -zxvf - -C /usr/bin/
+
+cilium install --version 1.16.4
+cilium status --wait
