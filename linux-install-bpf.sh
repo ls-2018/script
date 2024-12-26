3fb76edb21eda3ad91e0d9cc9898046dcaf4185c
FlyFishking <purple.boy@126.com>
2024-12-26 15:53:30 +0800

    add bpf
    Signed-off-by: FlyFishking <purple.boy@126.com>

--- linux-install-bpf.sh
+++ linux-install-bpf.sh
@@ -4,7 +4,7 @@ apt install wget -y
 # wget https://apt.llvm.org/llvm.sh
 # chmod +x llvm.sh
 # sudo ./llvm.sh 19 al
-
+cd ~
 wget https://mirrors.bfsu.edu.cn/llvm-apt/llvm.sh
 chmod +x llvm.sh
 ./llvm.sh 19 -m https://mirrors.bfsu.edu.cn/llvm-apt
@@ -29,6 +29,7 @@ apt-get install -y moreutils \
     pahole libcurl4-openssl-dev liblldb-dev gdb
 
 # libbpf-dev 可以替换为:
+cd ~
 git clone https://github.com/libbpf/libbpf.git
 cd libbpf/src && BUILD_STATIC_ONLY=y make install && cd - && rm -rf libbpf
 
@@ -39,17 +40,18 @@ if [ "x86_64" = $(arch) ]; then
 
     wget -O /usr/bin/ecli https://cf.ghproxy.cc/https://github.com/eunomia-bpf/eunomia-bpf/releases/download/v1.0.27/ecli
     wget -O /usr/bin/ecc https://cf.ghproxy.cc/https://github.com/eunomia-bpf/eunomia-bpf/releases/download/v1.0.27/ecc-x86_64
-
     wget -O - https://cf.ghproxy.cc/https://github.com/anakryiko/retsnoop/releases/download/v0.10.1/retsnoop-v0.10.1-amd64.tar.gz | tar -zxvf - && chmod +x ./retsnoop && mv retsnoop /usr/bin/
+    wget -O - https://cf.ghproxy.cc/https://github.com/cilium/pwru/releases/download/v1.0.9/pwru-linux-amd64.tar.gz | tar -zxvf - && chmod +x ./pwru && mv pwru /usr/bin/
 
 else
     wget -O /usr/bin/ecc https://cf.ghproxy.cc/https://github.com/eunomia-bpf/eunomia-bpf/releases/download/v1.0.27/ecc-aarch64
     wget -O /usr/bin/ecli https://cf.ghproxy.cc/https://github.com/eunomia-bpf/eunomia-bpf/releases/download/v1.0.27/ecli-aarch64
     wget -O - https://cf.ghproxy.cc/https://github.com/anakryiko/retsnoop/releases/download/v0.10.1/retsnoop-v0.10.1-arm64.tar.gz | tar -zxvf - && chmod +x ./retsnoop && mv retsnoop /usr/bin/
+    wget -O - https://cf.ghproxy.cc/https://github.com/cilium/pwru/releases/download/v1.0.9/pwru-linux-arm64.tar.gz | tar -zxvf - && chmod +x ./pwru && mv pwru /usr/bin/
 
 fi
 chmod +x /usr/bin/ec*
-
+cd ~
 git clone https://github.com/iovisor/bpftrace
 git config --global --unset http.postBuffer
 cp -R bpftrace bpftrace_scz
@@ -59,8 +61,9 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DALLOW_UNSAFE_PROBE:BOOL=ON -DCMAKE_PREFIX_
 make -j8
 make install
 bpftrace --info 2>&1 | grep bfd
-rm -rf ./*
+
 # apt-get install -y bpftrace
+cd ~
 
 git clone https://github.com/iovisor/bcc.git
 mkdir bcc/build
@@ -83,3 +86,13 @@ EOF
 cat <<EOF >>$HOME/.bashrc
 export PATH=\$PATH:/perf-tools/bin
 EOF
+
+cd ~
+wget https://cf.ghproxy.cc/https://www.tcpdump.org/release/libpcap-1.10.4.tar.gz
+tar -zxvf libpcap-1.10.4.tar.gz
+cd libpcap-1.10.4
+./configure --disable-rdma --disable-shared --disable-usb --disable-netmap --disable-bluetooth --disable-dbus --without-libnl
+make
+sudo make install
+
+rm -rf ./*
