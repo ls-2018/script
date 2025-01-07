f2591d1eec46345ddb2817f49d4fae9870f68b8b
acejilam <acejilam@gmail.com>
2025-01-07 15:54:39 +0800

    -

--- linux-install-k8s.sh
+++ linux-install-k8s.sh
@@ -6,7 +6,7 @@ ARCH=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 curl -sfL ${GITHUB_PROXY}/https://github.com/labring/sealos/releases/download/v${VERSION}/sealos_${VERSION}_linux_${ARCH}.tar.gz | tar -zxvf - -C /usr/bin/
 # sealos reset
 
-if [[$1 == "cilium"]]; then
+if [[ $1 == "cilium" ]]; then
   sealos run registry.cn-hangzhou.aliyuncs.com/acejilam/kubernetes-docker:v1.25.16 \
     registry.cn-hangzhou.aliyuncs.com/acejilam/helm:v3.8.2 \
     --masters 192.168.33.12 --passwd 'root'
