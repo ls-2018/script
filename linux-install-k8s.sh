53dd18b8b5a235142dccc7eb5d988db49b53ba96
acejilam <acejilam@gmail.com>
2025-01-07 15:06:52 +0800

    -

--- linux-install-k8s.sh
+++ linux-install-k8s.sh
@@ -6,22 +6,19 @@ ARCH=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 curl -sfL ${GITHUB_PROXY}/https://github.com/labring/sealos/releases/download/v${VERSION}/sealos_${VERSION}_linux_${ARCH}.tar.gz | tar -zxvf - -C /usr/bin/
 # sealos reset
 
-sealos run registry.cn-hangzhou.aliyuncs.com/acejilam/kubernetes-docker:v1.25.16 \
-  registry.cn-hangzhou.aliyuncs.com/acejilam/helm:v3.8.2 \
-  --masters 192.168.33.12 --passwd 'root'
+if [[$1 == "cilium"]]; then
+  sealos run registry.cn-hangzhou.aliyuncs.com/acejilam/kubernetes-docker:v1.25.16 \
+    registry.cn-hangzhou.aliyuncs.com/acejilam/helm:v3.8.2 \
+    --masters 192.168.33.12 --passwd 'root'
 
-sed -i "s#apiserver.cluster.local#$(hostname)#g" ~/.kube/config
-sed -i "s#kubernetes-admin@kubernetes#$(hostname)#g" ~/.kube/config
-cp -rf ~/.kube/config /.host_kube/$(hostname).config
+  curl -sfL ${GITHUB_PROXY}/https://github.com/cilium/cilium-cli/releases/download/v0.16.5/cilium-linux-${ARCH}.tar.gz | tar -zxvf - -C /usr/bin/
+  curl -sfL ${GITHUB_PROXY}/https://github.com/cilium/hubble/releases/download/v1.16.5/hubble-linux-${ARCH}.tar.gz | tar -zxvf - -C /usr/bin/
 
-curl -sfL ${GITHUB_PROXY}/https://github.com/cilium/cilium-cli/releases/download/v0.16.5/cilium-linux-${ARCH}.tar.gz | tar -zxvf - -C /usr/bin/
-curl -sfL ${GITHUB_PROXY}/https://github.com/cilium/hubble/releases/download/v1.16.5/hubble-linux-${ARCH}.tar.gz | tar -zxvf - -C /usr/bin/
+  cilium install --version 1.16.5
+  cilium status --wait
+  cilium hubble enable --relay --ui
 
-cilium install --version 1.16.5
-cilium status --wait
-cilium hubble enable --relay --ui
-
-kubectl apply -f - <<EOF
+  kubectl apply -f - <<EOF
 apiVersion: v1
 kind: Service
 metadata:
@@ -38,3 +35,15 @@ spec:
     k8s-app: hubble-ui
   type: NodePort
 EOF
+
+else
+
+  sealos run registry.cn-hangzhou.aliyuncs.com/acejilam/kubernetes-docker:v1.25.16 \
+    registry.cn-hangzhou.aliyuncs.com/acejilam/helm:v3.8.2 \
+    registry.cn-hangzhou.aliyuncs.com/acejilam/calico:v3.24.1 \
+    --masters 192.168.33.12 --passwd 'root'
+fi
+
+sed -i "s#apiserver.cluster.local#$(hostname)#g" ~/.kube/config
+sed -i "s#kubernetes-admin@kubernetes#$(hostname)#g" ~/.kube/config
+cp -rf ~/.kube/config /.host_kube/$(hostname).config
