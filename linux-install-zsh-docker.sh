adeddc2e9452e8ae859afe15e7fa11d2abf3aff0
acejilam <acejilam@gmail.com>
2025-05-18 19:15:45 +0800

    tags
    Signed-off-by: acejilam <acejilam@gmail.com>

--- linux-install-zsh-docker.sh
+++ linux-install-zsh-docker.sh
@@ -12,7 +12,7 @@ echo $SHELL
 
 echo y |sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone --depth=1https://github.com/romkatv/powerlevel10k.git        ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git        ~/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions              ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git      ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
