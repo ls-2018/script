1032c0400d428716c4106e4a74a7a7e64e9d79ad
acejilam <acejilam@gmail.com>
2025-09-04 11:14:24 +0800

    env
    Signed-off-by: acejilam <acejilam@gmail.com>

--- linux-add-env.sh
+++ linux-add-env.sh
@@ -8,20 +8,24 @@ wget -O ~/.customer_script.sh https://gitee.com/ls-2018/script/raw/main/.custome
 wget -O ~/.linux-env-bash.env https://gitee.com/ls-2018/script/raw/main/.linux-env-bash.env
 wget -O ~/.linux-env-zsh.env https://gitee.com/ls-2018/script/raw/main/.linux-env-zsh.env
 
-echo 'source ~/.customer_script.sh' | tee -a ~/.bashrc
-echo 'source ~/.linux-env-bash.env' | tee -a ~/.bashrc
+if [[ $(cat ~/.bashrc) =~ customer_script ]]; then
+	echo "customer_script exists"
+else
+	echo 'source ~/.customer_script.sh' | tee -a ~/.bashrc
+	echo 'source ~/.linux-env-bash.env' | tee -a ~/.bashrc
 
-echo 'source ~/.customer_script.sh' | tee -a /etc/profile
-echo 'source ~/.linux-env-bash.env' | tee -a /etc/profile
+	echo 'source ~/.customer_script.sh' | tee -a /etc/profile
+	echo 'source ~/.linux-env-bash.env' | tee -a /etc/profile
 
-echo 'source ~/.customer_script.sh' | tee -a ~/.zshrc
-echo 'source ~/.linux-env-zsh.env' | tee -a ~/.zshrc
+	echo 'source ~/.customer_script.sh' | tee -a ~/.zshrc
+	echo 'source ~/.linux-env-zsh.env' | tee -a ~/.zshrc
 
-echo 'source ~/.customer_script.sh' | tee -a ~/.zprofile
-echo 'source ~/.linux-env-zsh.env' | tee -a ~/.zprofile
+	echo 'source ~/.customer_script.sh' | tee -a ~/.zprofile
+	echo 'source ~/.linux-env-zsh.env' | tee -a ~/.zprofile
 
-echo 'source ~/.customer_script.sh' | tee -a ~/.zshenv
-echo 'source ~/.linux-env-zsh.env' | tee -a ~/.zshenv
+	echo 'source ~/.customer_script.sh' | tee -a ~/.zshenv
+	echo 'source ~/.linux-env-zsh.env' | tee -a ~/.zshenv
+fi
 
 # .zshenv ：最早加载的文件，在所有 zsh 实例中都会执行，包括非交互式、登录、脚本环境等。它在 zsh 启动早期执行，还没加载 compinit，也不具备交互式 shell 的功能。
 # .zshrc ：只在交互式 shell 中执行，通常用来设置 prompt、alias、函数、compdef 等 shell 功能。
