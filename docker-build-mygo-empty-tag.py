78ae72278e440685d06ce1bbaa034d5b036a3da6
acejilam <acejilam@gmail.com>
2025-12-17 10:09:31 +0800

    tags
    Signed-off-by: acejilam <acejilam@gmail.com>

--- docker-build-mygo-empty-tag.py
+++ docker-build-mygo-empty-tag.py
@@ -12,6 +12,7 @@ git tag -l
 ''').strip()
 os.system(f'cd {basedir} && git add . && git commit -s -m "tags" ')
 for tag in tags.split('\n'):
+    os.system(f'cd {basedir} && git push origin --delete tag {tag}')
     os.system(f'cd {basedir} && git tag -d {tag}')
     os.system(f'cd {basedir} && git tag {tag}')
 os.system(f'cd {basedir} && git push')
