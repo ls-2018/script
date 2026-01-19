0c70ed01f2ef0ad13d21add0d4ad662e8dbb1ca3
acejilam <acejilam@gmail.com>
2026-01-19 17:39:45 +0800

    -
    Signed-off-by: acejilam <acejilam@gmail.com>

---
+++ build-myrustbin.sh
@@ -0,0 +1,114 @@
+#! /usr/bin/env zsh
+expand() {
+	mkdir -p /tmp/cargo-expand/
+	echo '''
+# syntax=docker/dockerfile:1.7
+FROM ccr.ccs.tencentyun.com/ls-2018/myrust AS builder
+
+ARG TARGETARCH
+
+RUN if [ "$TARGETARCH" = "amd64" ]; then \
+        rustup target add x86_64-unknown-linux-musl ; \
+    elif [ "$TARGETARCH" = "arm64" ]; then \
+        rustup target add aarch64-unknown-linux-musl ; \
+    fi
+
+# 克隆代码
+RUN git clone https://github.com/dtolnay/cargo-expand.git
+WORKDIR /cargo-expand
+
+# 构建
+RUN if [ "$TARGETARCH" = "amd64" ]; then \
+        cargo build --release --target x86_64-unknown-linux-musl ; \
+    elif [ "$TARGETARCH" = "arm64" ]; then \
+        cargo build --release --target aarch64-unknown-linux-musl ; \
+    fi
+
+# 导出阶段
+FROM scratch AS export
+ARG TARGETARCH
+COPY --from=builder /cargo-expand/target/*/release/cargo-expand /cargo-expand
+''' >/tmp/cargo-expand/Dockerfile
+
+	cd /tmp/cargo-expand
+	docker buildx build --platform linux/amd64,linux/arm64 --output type=local,dest=./out .
+
+	mkdir -p /Users/acejilam/script/binary
+	cp -rf /tmp/cargo-expand/out/linux_amd64/cargo-expand /Users/acejilam/script/binary/cargo-expand-x86_64
+	cp -rf /tmp/cargo-expand/out/linux_arm64/cargo-expand /Users/acejilam/script/binary/cargo-expand-aarch64
+
+	md5sum /tmp/cargo-expand/out/linux_amd64/cargo-expand
+	md5sum /Users/acejilam/script/binary/cargo-expand-x86_64
+
+	md5sum /tmp/cargo-expand/out/linux_arm64/cargo-expand
+	md5sum /Users/acejilam/script/binary/cargo-expand-aarch64
+
+}
+
+#expand
+
+generate() {
+	mkdir -p /tmp/cargo-generate/
+	echo '''
+# syntax=docker/dockerfile:1.7
+FROM ccr.ccs.tencentyun.com/ls-2018/myrust AS builder
+
+ARG TARGETARCH
+
+RUN if [ "$TARGETARCH" = "amd64" ]; then \
+        rustup target add x86_64-unknown-linux-musl ; \
+    elif [ "$TARGETARCH" = "arm64" ]; then \
+        rustup target add aarch64-unknown-linux-musl ; \
+    fi
+
+# 克隆代码
+RUN git clone https://github.com/cargo-generate/cargo-generate.git
+WORKDIR /cargo-generate
+
+RUN apt install pkg-config libssl-dev musl-tools zlib1g-dev -y
+RUN cargo add openssl-sys --features vendored
+
+# 构建
+RUN if [ "$TARGETARCH" = "amd64" ]; then \
+        cargo build --release --target x86_64-unknown-linux-musl ; \
+    elif [ "$TARGETARCH" = "arm64" ]; then \
+        cargo build --release --target aarch64-unknown-linux-musl ; \
+    fi
+
+# 导出阶段
+FROM scratch AS export
+ARG TARGETARCH
+COPY --from=builder /cargo-generate/target/*/release/cargo-generate /cargo-generate
+''' >/tmp/cargo-generate/Dockerfile
+
+	cd /tmp/cargo-generate
+	docker buildx build --platform linux/amd64,linux/arm64 --output type=local,dest=./out .
+
+	mkdir -p /Users/acejilam/script/binary
+	cp -rf /tmp/cargo-generate/out/linux_amd64/cargo-generate /Users/acejilam/script/binary/cargo-generate-x86_64
+	cp -rf /tmp/cargo-generate/out/linux_arm64/cargo-generate /Users/acejilam/script/binary/cargo-generate-aarch64
+
+	md5sum /tmp/cargo-generate/out/linux_amd64/cargo-generate
+	md5sum /Users/acejilam/script/binary/cargo-generate-x86_64
+
+	md5sum /tmp/cargo-generate/out/linux_arm64/cargo-generate
+	md5sum /Users/acejilam/script/binary/cargo-generate-aarch64
+
+}
+
+generate
+
+sccahe() {
+	temp_dir=$(mktemp -d)
+	eval "$(print_proxy.py)"
+	wget -q -nv https://github.com/mozilla/sccache/releases/download/v0.13.0/sccache-v0.13.0-aarch64-unknown-linux-musl.tar.gz
+	tar -zxf sccache-v0.13.0-aarch64-unknown-linux-musl.tar.gz
+	mv sccache-v0.13.0-aarch64-unknown-linux-musl/sccache /Users/acejilam/script/binary/sccache-aarch64
+	wget -q -nv https://github.com/mozilla/sccache/releases/download/v0.13.0/sccache-v0.13.0-x86_64-unknown-linux-musl.tar.gz
+	tar -zxf sccache-v0.13.0-x86_64-unknown-linux-musl.tar.gz
+	mv sccache-v0.13.0-x86_64-unknown-linux-musl/sccache /Users/acejilam/script/binary/sccache-x86_64
+	cd -
+	rm -rf ${temp_dir}
+}
+
+sccahe
